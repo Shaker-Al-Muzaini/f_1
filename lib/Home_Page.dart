c07480d3d25335f246c480cfd5278e04dd621a05
shaker almazini <92122042+1shaker@users.noreply.github.com>
2022-04-28 16:19:44 +0300

    ASSIGNMENT 1
    ASSIGNMENT 1

--- lib/Home_Page.dart
+++ lib/Home_Page.dart
@@ -2,9 +2,47 @@
 
 import 'package:flutter/material.dart';
 
-class Home_Page extends StatelessWidget{
-  const Home_Page({Key? key}) : super(key: key);
+class Date{
+  late final String name;
+  late final String opject;
 
+  Date({
+    required this.name,
+    required this.opject,
+
+});
+}
+
+
+// ignore: camel_case_types, must_be_immutable
+class Home_Page extends StatelessWidget{
+
+// ignore: non_constant_identifier_names
+List<Date>DeteInfo=[
+  Date(
+    name:'الاسم',
+    opject:'القدس',
+  ),
+  Date(
+    name:'المساحة',
+    opject:'125كم',
+  ),
+  Date(
+    name:'السكان',
+    opject:'358000نسمه',
+  ),
+  Date(
+    name:'الدولة',
+    opject:'فلسطين',
+  ),
+  Date(
+    name:'الطالب',
+    opject:'شاكر المزيني',
+  ),
+
+];
+
+  Home_Page({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +60,8 @@ class Home_Page extends StatelessWidget{
       ),
 
       body:SingleChildScrollView(
-        
         child: Column(
           children:[
-      
             Container(
               decoration:const BoxDecoration(
                 borderRadius:BorderRadiusDirectional.only(
@@ -39,7 +75,7 @@ class Home_Page extends StatelessWidget{
                     width:double.infinity,
                     image:AssetImage(
                       'images/j.png'
-      
+
                     )
                   ),
                   Container(
@@ -57,336 +93,95 @@ class Home_Page extends StatelessWidget{
                         fontSize:44.0,
                         fontFamily:'cairo',
                       ),
-      
+
                     ),
                   ),
                 ],
-      
+
               ),
-      
+
             ),
-           
             const Text('مدينة القدس ',
             style:TextStyle(
               fontSize:24,
-              fontFamily:'cairo',
-      
+              fontFamily:'cairo', )),
+            const SizedBox(height:1),
+            ListView.separated(
+              shrinkWrap:true,
+              itemBuilder:(context,index)=>Deta(DeteInfo[index]),
+              separatorBuilder:(context,index)
+              =>const SizedBox (height:1),
+              itemCount:DeteInfo.length,
             )
-      
-            ),
-           
-            Container(
-              margin: const EdgeInsets.all(15),
-              decoration: const BoxDecoration(
-                 color: Color.fromARGB(58, 197, 197, 197),
-                 border: Border(left:BorderSide(color:Colors.black,width:2),right:BorderSide(color:Colors.white,width:2),
-                 top:BorderSide(color:Colors.red,width:2),bottom:BorderSide(color:Colors.green,width:2),)
-      
-              ),
-      
-              width:double.infinity,
-              child: Row(
-                children: [
-                  Expanded(
-                    child:  Container(
-                       margin: const EdgeInsets.all(8),
-                        decoration:BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                            border:Border.all(color:Colors.black,width:2),
-                        ),
-                      child: const Padding(
-                        padding: EdgeInsetsDirectional.only(
-                          start: 50,
-                        ),
-                        child: Text(
-                          'القدس', //  textWidthBasis:,
-                        textAlign: TextAlign.center,
-                        style:TextStyle(
-                          fontSize: 30,
-                          fontFamily:'cairo',
-                          color:Colors.blueGrey,
-                        ),
-                        ),
-                      ),
-                    ),
-                  ),
-                   Container(
-                      margin: const EdgeInsets.all(8),
-                       decoration:BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border:Border.all(color:Colors.black,width:2),
-                      ),
-                    child:const Expanded(
-                      child:  Padding(
-                        padding: EdgeInsetsDirectional.only(
-                          start: 40,
-                        ),
-                        child: Text(
-                          'الاسم',
-                           textAlign: TextAlign.center,
-                           style:TextStyle(
-                             color:Colors.blueAccent,
-                             fontSize: 30,
-                           )
-                        ),
-                      ),
-                    ),
-                  ),    
-                ]
-              )
-            ),
-      
-      
-            Container(
-        margin: const EdgeInsets.all(15),
-        decoration: const BoxDecoration(
-           color: Color.fromARGB(58, 197, 197, 197),
-           border: Border(left:BorderSide(color:Colors.black,width:2),right:BorderSide(color:Colors.white,width:2),
-           top:BorderSide(color:Colors.red,width:2),bottom:BorderSide(color:Colors.green,width:2),)
-        ),
-        width:double.infinity,
-        child: Row(
-          children: [
-        Expanded(
-          child:  Container(
-             margin: const EdgeInsets.all(8),
-              decoration:BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                  border:Border.all(color:Colors.black,width:2),
-              ),
-            child: const Padding(
-              padding: EdgeInsetsDirectional.only(
-                start: 50,
-              ),
-              child: Text(
-             'كم125', //  textWidthBasis:,
-              textAlign: TextAlign.center,
-              style:TextStyle(
-                fontSize: 30,
-                fontFamily:'cairo',
-                color:Colors.blueGrey,
-              ),
-              ),
-            ),
-          ),
-        ),
-         Container(
-            margin: const EdgeInsets.all(8),
-             decoration:BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border:Border.all(color:Colors.black,width:2),
-            ),
-          child:const Expanded(
-            child:  Padding(
-              padding: EdgeInsetsDirectional.only(
-                start: 40,
-              ),
-              child: Text(
-                'المساحة',
-                 textAlign: TextAlign.center,
-                 style:TextStyle(
-                   color:Colors.blueAccent,
-                   fontSize: 30,
-                 )
-              ),
-            ),
-          ),
-        ),    
-          ]
-        )
-      ),
-      
-      
-      
-      Container(
-        margin: const EdgeInsets.all(15),
-        decoration: const BoxDecoration(
-           color: Color.fromARGB(58, 197, 197, 197),
-           border: Border(left:BorderSide(color:Colors.black,width:2),right:BorderSide(color:Colors.white,width:2),
-           top:BorderSide(color:Colors.red,width:2),bottom:BorderSide(color:Colors.green,width:2),)
-        ),
-        width:double.infinity,
-        child: Row(
-          children: [
-        Expanded(
-          child:  Container(
-             margin: const EdgeInsets.all(8),
-              decoration:BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                  border:Border.all(color:Colors.black,width:2),
-              ),
-            child: const Padding(
-              padding: EdgeInsetsDirectional.only(
-                start: 50,
-              ),
-              child: Text(
-                'نسمة358000', //  textWidthBasis:,
-              textAlign: TextAlign.center,
-              style:TextStyle(
-                fontSize: 30,
-                fontFamily:'cairo',
-                color:Colors.blueGrey,
-              ),
-              ),
-            ),
-          ),
-        ),
-         Container(
-            margin: const EdgeInsets.all(8),
-             decoration:BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border:Border.all(color:Colors.black,width:2),
-            ),
-          child:const Expanded(
-            child:  Padding(
-              padding: EdgeInsetsDirectional.only(
-                start: 40,
-              ),
-              child: Text(
-                'السكان',
-                 textAlign: TextAlign.center,
-                 style:TextStyle(
-                   color:Colors.blueAccent,
-                   fontSize: 30,
-                 )
-              ),
-            ),
-          ),
-        ),    
-          ]
-        )
-      ),
-      
-      
-      
-      
-      Container(
-        margin: const EdgeInsets.all(15),
-        decoration: const BoxDecoration(
-           color: Color.fromARGB(58, 197, 197, 197),
-           border: Border(left:BorderSide(color:Colors.black,width:2),right:BorderSide(color:Colors.white,width:2),
-           top:BorderSide(color:Colors.red,width:2),bottom:BorderSide(color:Colors.green,width:2),)
-        ),
-        width:double.infinity,
-        child: Row(
-          children: [
-        Expanded(
-          child:  Container(
-             margin: const EdgeInsets.all(8),
-              decoration:BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                  border:Border.all(color:Colors.black,width:2),
-              ),
-            child: const Padding(
-              padding: EdgeInsetsDirectional.only(
-                start: 50,
-              ),
-              child: Text(
-                'فلسطين ', //  textWidthBasis:,
-              textAlign: TextAlign.center,
-              style:TextStyle(
-                fontSize: 30,
-                fontFamily:'cairo',
-                color:Colors.blueGrey,
-              ),
-              ),
-            ),
-          ),
-        ),
-         Container(
-            margin: const EdgeInsets.all(8),
-             decoration:BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border:Border.all(color:Colors.black,width:2),
-            ),
-          child:const Expanded(
-            child:  Padding(
-              padding: EdgeInsetsDirectional.only(
-                start: 40,
-              ),
-              child: Text(
-                'الدولة',
-                 textAlign: TextAlign.center,
-                 style:TextStyle(
-                   color:Colors.blueAccent,
-                   fontSize: 30,
-                 )
-              ),
-            ),
-          ),
-        ),    
-          ]
-        )
-      ),
-      
-      
-      Container(
-        margin: const EdgeInsets.all(15),
-        decoration: const BoxDecoration(
-           color: Color.fromARGB(58, 197, 197, 197),
-           border: Border(left:BorderSide(color:Colors.black,width:2),right:BorderSide(color:Colors.white,width:2),
-           top:BorderSide(color:Colors.red,width:2),bottom:BorderSide(color:Colors.green,width:2),)
-        ),
-        width:double.infinity,
-        child: Row(
-          children: [
-        Expanded(
-          child:  Container(
-             margin: const EdgeInsets.all(8),
-              decoration:BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                  border:Border.all(color:Colors.black,width:2),
-              ),
-            child: const Padding(
-              padding: EdgeInsetsDirectional.only(
-                start: 50,
-              ),
-              child: Text(
-                'شاكر المزيني', //  textWidthBasis:,
-              textAlign: TextAlign.center,
-              style:TextStyle(
-                fontSize: 30,
-                fontFamily:'cairo',
-                color:Colors.blueGrey,
-              ),
-              ),
-            ),
-          ),
-        ),
-         Container(
-            margin: const EdgeInsets.all(8),
-             decoration:BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border:Border.all(color:Colors.black,width:2),
-            ),
-          child:const Expanded(
-            child:  Padding(
-              padding: EdgeInsetsDirectional.only(
-                start: 40,
-              ),
-              child: Text(
-                'الطالب',
-                 textAlign: TextAlign.center,
-                 style:TextStyle(
-                   color:Colors.blueAccent,
-                   fontSize: 30,
-                 )
-              ),
-            ),
-          ),
-        ),    
-          ]
-        )
-      ),
-      
           ],
-      
+
         ),
       )
 
     );
 
-
   }
+
+  // ignore: non_constant_identifier_names
+  Widget Deta(Date D)=>Container(
+      margin: const EdgeInsets.all(15),
+      decoration: const BoxDecoration(
+          color: Color.fromARGB(58, 197, 197, 197),
+          border: Border(left:BorderSide(color:Colors.black,width:2),right:BorderSide(color:Colors.white,width:2),
+            top:BorderSide(color:Colors.red,width:2),bottom:BorderSide(color:Colors.green,width:2),)
+
+      ),
+
+      width:double.infinity,
+      child: Row(
+          children: [
+            Expanded(
+              child:  Container(
+                margin: const EdgeInsets.all(8),
+                decoration:BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border:Border.all(color:Colors.black,width:2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.only(
+                    start: 50,
+                  ),
+                  child: Text(
+                    D.opject, //  textWidthBasis:,
+                    textAlign: TextAlign.center,
+                    style:const TextStyle(
+                      fontSize: 30,
+                      fontFamily:'cairo',
+                      color:Colors.blueGrey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(8),
+              decoration:BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border:Border.all(color:Colors.black,width:2),
+              ),
+              child: Expanded(
+                child:  Padding(
+                  padding: const EdgeInsetsDirectional.only(
+                    start: 40,
+                  ),
+                  child: Text(
+                      D.name,
+                      textAlign: TextAlign.center,
+                      style:const TextStyle(
+                        color:Colors.blueAccent,
+                        fontSize: 30,
+                      )
+                  ),
+                ),
+              ),
+            ),
+          ]
+      )
+  );
 
 }
